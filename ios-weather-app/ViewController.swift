752a43799aa5254dfcbebc391b8c93b8976bb0bb
Igor Elkanyuk <ielkanyuk@gmail.com>
2020-11-22 15:58:46 +0500

    Initial Commit

---
+++ ios-weather-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-weather-app
+//
+//  Created by Igor Elkanyuk on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
